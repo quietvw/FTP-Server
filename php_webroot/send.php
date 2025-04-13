ed43d3e236ed85774366c0eb7c605882c80a7838
Kush Patel <kushpatelj@Kushs-MacBook-Air-2.local>
2025-04-12 19:25:23 -0700

    added file sharing and it adds to the server just need to debug

--- php_webroot/send.php
+++ php_webroot/send.php
@@ -1,8 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Send the File</title>
+</head>
+<body>
 <?php
-    $myPDO = new PDO('sqlite:../database.db');
+$myPDO = new PDO('sqlite:../database.db');
 $result = $myPDO->query("SELECT * FROM users");
-foreach($result as $row)
-{
+foreach ($result as $row) {
     var_dump($row);
 }
+
+if (isset($_FILES['File']) && $_FILES['File']['error'] === UPLOAD_ERR_OK) {
+    $uploadDir = '../uploads/root/admin/';
+    $filename = basename($_FILES['File']['name']);
+    $targetPath = $uploadDir . $filename;
+
+    // Make sure the upload directory exists
+    if (!is_dir($uploadDir)) {
+        mkdir($uploadDir, 0755, true);
+    }
+
+    // Move uploaded file to desired location
+    if (move_uploaded_file($_FILES['File']['tmp_name'], $targetPath)) {
+        echo "<h2>File uploaded successfully!</h2>";
+
+        // Use download.php to serve the file with correct MIME type
+        $encodedFilename = urlencode($filename);
+        echo "<p><a href='download.php?file=$encodedFilename'>Click here to download $filename</a></p>";
+    } else {
+        echo "<p>Error moving the uploaded file.</p>";
+    }
+} else {
+    echo "<p>No file uploaded or there was an upload error.</p>";
+}
 ?>
+</body>
+</html>
