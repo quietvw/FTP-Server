24352dc86a418e22bc432ddb710119a57711bbdd
JaredSkates <jaredskatesz12@gmail.com>
2025-04-12 20:23:31 -0700

    Login UI Fixed

--- php_webroot/login.php
+++ php_webroot/login.php
@@ -19,18 +19,18 @@ if ($_POST["username"] == "admin" && $_POST["password"] == "admin") {
     <link rel="stylesheet" href="assets/root.css">
     <link rel="stylesheet" href="dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.js"></script>
 
     <title>FTP Server</title>
   </head>
 
   <body>
       <div class="p-4 flex flex-col justify-center items-center h-screen">
-          <form class="bg-white border-1 border-black p-2 shadow-lg shadow-white w-[35%] h-80 font-thin flex flex-col justify-center" method="POST" >
-              <?php if (isset($error)) { ?> <br>The username/password is invalid. <br><?php } ?>
-          Username <br> <input class="p-1 mt-1 border-1 border-black" type="text" name="username"><br>
+          <h1 class="absolute text-[50px] text-center font-thin text-white -translate-y-[270%] translate-x-2 tracking-[0.4em] italic">Nebula</h1>
+          <form class="bg-white p-2 shadow-lg shadow-black w-[35%] h-80 font-thin flex flex-col justify-center" method="POST" >
+              <?php if (isset($error)) { ?> <p class="warningText">The username/password is invalid.</p> <br><?php } ?>
+          Username <br> <input class="icon-user p-1 mt-1 border-1 border-black" type="text" name="username"><br>
           Password <br> <input class="p-1 mt-1 border-1 border-black" type="password" name="password"><br>
-              <input class="mt-2 p-2 border-1 bg-blue-100 rounded-md" type="submit" value="Login">
+              <input class="mt-2 p-2 border-1 bg-gradient-to-r from-blue-100 to-green-100  rounded-md font-bold" type="submit" value="Login">
           </form>
       </div>
 
